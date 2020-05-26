85e496b262e9e7d3554d0ef7f8ba74b83495fabd
Peter Askey <peter_g_askey@live.co.uk>
2020-05-26 11:43:02 +0200

    removed unused notYetImplemented function

--- src/main/java/org/alduthir/App.java
+++ src/main/java/org/alduthir/App.java
@@ -77,19 +77,4 @@ public class App extends Application {
         stage.setTitle("Full Steam Drum Machine");
         stage.show();
     }
-
-    /**
-     * In case functionality for a button is not yet implemented. Display an Alert to the user.
-     */
-    @FXML
-    protected void notYetImplemented() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        DialogPane dialogPane = alert.getDialogPane();
-        dialogPane.getStylesheets().add(
-                App.class.getResource("styles.css").toExternalForm());
-        dialogPane.getStyleClass().add("fx-dialog");
-        alert.setTitle("Not yet implemented");
-        alert.setContentText("Deze functionaliteit is nog niet geïmplementeerd.");
-        alert.showAndWait();
-    }
 }
